bad061f038ed22a2c71bdb70df2fdff25804cb3b
Amanda Baker <t-ambak@microsoft.com>
2016-07-19 09:57:15 -0700

    Reformat Code

--- node-web-app/server.js
+++ node-web-app/server.js
@@ -1,40 +1,41 @@
 // server.js
 
-'use strict';
+'use strict'
 
-const express         = require('express');
-const app             = express();
-const bodyParser      = require('body-parser');
-const methodOverride  = require('method-override');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // Configuration
 
-const db              = require('./config/db');
-const PORT = 80;
+const db = require('./config/db')
+const PORT = 80
 
-// connect to our mongoDB database 
-mongoose.connect(db.url);
+// connect to our mongoDB database
+mongoose.connect(db.url)
 
 // get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json()); 
+// parse application/json
+app.use(bodyParser.json())
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'))
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'))
 
 // routes ==================================================
-require('./app/routes')(app); // configure our routes
+require('./app/routes')(app) // configure our routes
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT)
 
-exports = module.exports = app;
+exports = module.exports = app
