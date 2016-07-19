2a3c474be968961ac58c772fef78a3e3625c7443
Amanda Baker <t-ambak@microsoft.com>
2016-07-19 12:35:27 -0700

    Add API routes for post and getByID

--- node-web-app/app/routes.js
+++ node-web-app/app/routes.js
@@ -1,44 +1,93 @@
 // app/routes.js
 
 // grab the nerd model we just created
-var Nerd = require('./models/nerd');
+var Nerd = require('./models/nerd')
+var nerd = 0
 
 module.exports = function (app) {
-
   // server routes ===========================================================
-  // handle things like api calls
-  // authentication routes
 
   // sample api route
   app.get('/api/nerds', function (req, res) {
     // use mongoose to get all nerds in the database
     Nerd.find(function (err, nerds) {
-
-      // if there is an error retrieving, send the error. 
+      // if there is an error retrieving, send the error.
       // nothing after res.send(err) will execute
-      if (err)
-        res.send(err);
+      if (err) {
+        res.send(err)
+        return
+      }
+      console.log(nerds)
+      res.json(nerds) // return all nerds in JSON format
+      return
+    })
+  })
 
-      res.json(nerds); // return all nerds in JSON format
-    });
-  });
+  app.get('/api/nerds/:id', function (req, res) {
+    // use mongoose to get all nerds in the database
+    console.log(req.params.id)
+    console.log('now im going to enter the possibly forever while loop')
+    var interval = setInterval(function () {
+      getOneNerd(req.params.id)
+      setTimeout(function () {
+        if (nerd && nerd !== '' && nerd !== 1) {
+          console.log(nerd)
+          res.json(nerd)
+          clearInterval(interval)
+          nerd = 0
+        }
+      }, 10)
+    }, 15
+    )
+  })
 
   // route to handle creating goes here (app.post)
-  app.post('/api/nerds', function (req,res) {
-    var thing = req.body;
-    console.log(req);
-    Nerd.create (thing, function (err, thing) {
-      if (err) return handleError (err);
-      
-     res.json(thing);
-    });
-  });
-  // route to handle delete goes here (app.delete)
+  app.post('/api/nerds', function (req, res) {
+    var thing = {
+      name: req.body.data
+    }
+    console.log(thing)
+    Nerd.create(thing, function (err, thingObject) {
+      if (err) {
+        console.log('Things went awry during object creation')
+      }
+      console.log(thingObject)
+      res.json(thingObject)
+    })
+  })
 
   // frontend routes =========================================================
   // route to handle all angular requests
   app.get('*', function (req, res) {
-    res.sendfile('./public/views/index.html'); // load our public/index.html file
-  });
+    res.sendFile('/usr/src/app/public/views/index.html') // load our public/index.html file
+  })
 
-};
+  function getOneNerd (nerdID) {
+    Nerd.find(function (err, nerds) {
+      if (err) {
+        console.log('wut')
+        return err
+      }
+      console.log(nerds)
+      nerds = nerds.filter(filterByID)
+      if (nerds.length > 0) {
+        // Return the first one so you dont return an array
+        console.log(nerds[0])
+        nerd = nerds[0]
+      }
+      console.log('theres nothing here')
+      return 1
+    })
+
+    function filterByID (obj) {
+      var objID = obj._id
+      console.log(objID)
+      // Triple equals does not work here
+      if (nerdID == objID) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
