396907812388db10959bbb3afb27042c9f3d8630
Amanda Baker <t-ambak@microsoft.com>
2016-07-19 12:36:38 -0700

    Update get and post services to work with API

--- node-web-app/public/js/services/NerdService.js
+++ node-web-app/public/js/services/NerdService.js
@@ -1,24 +1,28 @@
-  // public/js/services/NerdService.js
-  
-angular.module('NerdService', []).factory('Nerd', ['$http', function ($http) {
+// public/js/services/NerdService.js
 
+angular.module('NerdService', []).factory('Nerd', ['$http', function ($http) {
   return {
     // call to get all nerds
     get: function () {
-      return $http.get('/api/nerds');
+      return $http.get('/api/nerds')
     },
-
-
+    // call to a specific nerd
+    getOne: function (id) {
+      return $http.get('/api/nerds/' + id)
+    },
     // these will work when more API routes are defined on the Node side of things
     // call to POST and create a new nerd
     create: function (nerdData) {
-      return $http.post('/api/nerds', nerdData);
+      var config = {
+        method: 'POST',
+        data: nerdData
+      }
+      return $http.post('/api/nerds', config)
     },
 
     // call to DELETE a nerd
     delete: function (id) {
-      return $http.delete('/api/nerds/' + id);
+      return $http.delete('/api/nerds/' + id)
     }
   }
-
-}]);
+}])
