caf50769c30b847fca3a6a910f31baa362e446b2
Amanda Baker <t-ambak@microsoft.com>
2016-07-19 12:25:45 -0700

    Save and retrieve string to/from database

--- node-web-app/public/js/controllers/MainCtrl.js
+++ node-web-app/public/js/controllers/MainCtrl.js
@@ -1,7 +1,32 @@
 // public/js/controllers/MainCtrl.js
 
-angular.module('MainCtrl', []).controller('MainController', function ($scope) {
-
-  $scope.tagline = 'To the moon and back!';
-
-});
+angular.module('MainCtrl', []).controller('MainController', ['$scope', 'Nerd', function ($scope, Nerd) {
+  $scope.tagline = 'To the moon and back!'
+  $scope.randomText = ''
+  $scope.reverse = ''
+  $scope.nerds = []
+  $scope.getReverse = function (randomText) {
+    console.log('Starting getReverse')
+    $scope.tagline = 'Anything other than what it said before!'
+    Nerd.create($scope.randomText)
+      .then(function (createdObject) {
+        createdObject = createdObject.data
+        console.log(createdObject)
+        Nerd.getOne(createdObject._id)
+          .then(function (nerd) {
+            nerd = nerd.data
+            console.log(nerd)
+            $scope.reverse = reverseString(nerd.name)
+            console.log($scope.reverse)
+            console.log($scope.reverse)
+          }, function (err) {
+            console.log('Could not fetch the string')
+          })
+      }, function (err) {
+        console.log('Could not save the string')
+      })
+  }
+  function reverseString (s) {
+    return s.split('').reverse().join('')
+  }
+}])
